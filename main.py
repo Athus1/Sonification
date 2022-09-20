00a54e642d01b9dbd2bee9d246d7e004b2cdf121
Atharv Allepally <atharvabhi@protonmail.com>
2022-09-20 12:53:10 -0700

    moving data to seperate df

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import pandas as pd
 
-overdosedf = pd.read_csv(r"C:\Users\athar\Documents\Python Scripts\overdose data.csv")
+overdosedf = pd.read_csv(r"overdose data.csv")
 overdosedf = overdosedf.replace(',', '', regex=True)
 
-#print(overdosedf)
+# print(overdosedf)
 #print(overdosedf["Year"])
 
-
 beats = overdosedf["Year"].count() - 1
-print(beats)
+#print(beats)
 
+mididict = {'pitch': [], 'instrument': []}
+mididf = pd.DataFrame(data=mididict)
+a = 0
+while beats > 0:
+    mididf.loc[len(mididf.index)] = [overdosedf["Opioids"][a], "test"]
+    beats = beats - 1
+    a = a + 1
+print(mididf)
